bb871a11a471abdb464c8ca06f40fa649de2c9aa
Zak Strassberg <zakstrassberg@gmail.com>
2025-01-03 13:50:33 -0800

    test config

--- hosts/fridge/configuration.nix
+++ hosts/fridge/configuration.nix
@@ -95,19 +95,19 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    ports = [22];
-    settings = {
-      PasswordAuthentication = true;
-    };
-  };
+  # services.openssh = {
+  #   enable = true;
+  #   ports = [22];
+  #   settings = {
+  #     PasswordAuthentication = true;
+  #   };
+  # };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
