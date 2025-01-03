9ed93a2fb78e91d21639d2dd61be2e2977b90841
Zak Strassberg <zakstrassberg@gmail.com>
2025-01-03 15:42:04 -0800

    test config

--- modules/user.nix
+++ modules/user.nix
@@ -15,32 +15,28 @@ with lib; {
       description = "zak";
       extraGroups = ["networkmanager" "wheel" "input" "audio" "video" "storage"];
       isNormalUser = true;
-      home = "/home/strass";
-      group = "strass";
       uid = 1000;
     };
 
        users.groups.strass = {};
 
-    # users.users.${config.user.name} = mkAliasDefinitions options.user;
-
     home-manager.useUserPackages = true;
     home-manager.useGlobalPkgs = true;
 
-    # # hm.home.username = config.user.name;
-    # hm.home.homeDirectory = lib.mkForce config.user.home;
+    hm.home.username = "strass";
+    hm.home.homeDirectory = "/home/strass";
 
-    # nix.settings = let
-    #   users = ["root" config.user.name];
-    # in {
-    #   trusted-users = users;
-    #   allowed-users = users;
-    # };
+    nix.settings = let
+      users = ["root" config.user.name];
+    in {
+      trusted-users = users;
+      allowed-users = users;
+    };
 
-    # users.users.root = {
-    #   packages = [ pkgs.shadow ];
-    #   shell = pkgs.shadow;
-    #   # hashedPassword = "!";
-    # };
+    users.users.root = {
+      packages = [ pkgs.shadow ];
+      shell = pkgs.shadow;
+      # hashedPassword = "!";
+    };
   };
 }
