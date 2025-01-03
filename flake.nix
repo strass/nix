128dbd4300fd44aa69361018e4e7e69cb2607243
Zak Strassberg <zakstrassberg@gmail.com>
2025-01-03 13:17:43 -0800

    fix path

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.strass = import ./home.nix;
+            home-manager.users.strass = import ./modules/home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
           }
