eeb8f95f948e996b22a74e81d922b24d8ca7bc40
Zak Strassberg <zakstrassberg@gmail.com>
2025-01-03 16:09:03 -0800

    test config

--- modules/niri.nix
+++ modules/niri.nix
@@ -41,8 +41,8 @@ in {
           #workspace-auto-back-and-forth = true;
 
           keyboard.xkb = {
-            layout = "us,us";
-            variant = ",workman";
+            layout = "us";
+            variant = "";
             options = "grp:win_space_toggle";
           };
 
@@ -197,116 +197,7 @@ in {
           "Mod+Down".action  = focus-window-down;
           "Mod+Up".action    = focus-window-up;
           "Mod+Right".action = focus-column-right;
-          #"Mod+H".action     = focus-column-left;
-          #"Mod+J".action     = focus-window-down;
-          #"Mod+K".action     = focus-window-up;
-          #"Mod+L".action     = focus-column-right;
-
-          "Mod+Shift+Left".action  = move-column-left;
-          "Mod+Shift+Down".action  = move-window-down;
-          "Mod+Shift+Up".action    = move-window-up;
-          "Mod+Shift+Right".action = move-column-right;
-          #"Mod+Shift+H".action     = move-column-left;
-          #"Mod+Shift+J".action     = move-window-down;
-          #"Mod+Shift+K".action     = move-window-up;
-          #"Mod+Shift+L".action     = move-column-right;
-
-          # "Mod+J".action     = focus-window-or-workspace-down;
-          # "Mod+K".action     = focus-window-or-workspace-up;
-          # "Mod+Ctrl+J".action     = move-window-down-or-to-workspace-down;
-          # "Mod+Ctrl+K".action     = move-window-up-or-to-workspace-up;
-
-          "Mod+Home".action = focus-column-first;
-          "Mod+End".action  = focus-column-last;
-          "Mod+Shift+Home".action = move-column-to-first;
-          "Mod+Shift+End".action  = move-column-to-last;
-
-          #"Mod+Shift+Left".action  = focus-monitor-left;
-          #"Mod+Shift+Down".action  = focus-monitor-down;
-          #"Mod+Shift+Up".action    = focus-monitor-up;
-          #"Mod+Shift+Right".action = focus-monitor-right;
-          #"Mod+Shift+H".action     = focus-monitor-left;
-          #"Mod+Shift+J".action     = focus-monitor-down;
-          #"Mod+Shift+K".action     = focus-monitor-up;
-          #"Mod+Shift+L".action     = focus-monitor-right;
-
-          #"Mod+Shift+Ctrl+Left".action  = move-column-to-monitor-left;
-          #"Mod+Shift+Ctrl+Down".action  = move-column-to-monitor-down;
-          #"Mod+Shift+Ctrl+Up".action    = move-column-to-monitor-up;
-          #"Mod+Shift+Ctrl+Right".action = move-column-to-monitor-right;
-          #"Mod+Shift+Ctrl+H".action     = move-column-to-monitor-left;
-          #"Mod+Shift+Ctrl+J".action     = move-column-to-monitor-down;
-          #"Mod+Shift+Ctrl+K".action     = move-column-to-monitor-up;
-          #"Mod+Shift+Ctrl+L".action     = move-column-to-monitor-right;
-
-          "Mod+Page_Down".action      = focus-workspace-down;
-          "Mod+Page_Up".action        = focus-workspace-up;
-          #"Mod+U".action              = focus-workspace-down;
-          #"Mod+I".action              = focus-workspace-up;
-          "Mod+Shift+Page_Down".action = move-column-to-workspace-down;
-          "Mod+Shift+Page_Up".action   = move-column-to-workspace-up;
-          #"Mod+Ctrl+U".action         = move-column-to-workspace-down;
-          #"Mod+Ctrl+I".action         = move-column-to-workspace-up;
-
-          "Mod+Ctrl+Page_Down".action = move-workspace-down;
-          "Mod+Ctrl+Page_Up".action   = move-workspace-up;
-          #"Mod+Shift+U".action         = move-workspace-down;
-          #"Mod+Shift+I".action         = move-workspace-up;
-
-          "Mod+1".action = focus-workspace 1;
-          "Mod+2".action = focus-workspace 2;
-          "Mod+3".action = focus-workspace 3;
-          "Mod+4".action = focus-workspace 4;
-          "Mod+5".action = focus-workspace 5;
-          "Mod+6".action = focus-workspace 6;
-          "Mod+7".action = focus-workspace 7;
-          "Mod+8".action = focus-workspace 8;
-          "Mod+9".action = focus-workspace 9;
-          "Mod+Shift+1".action = move-column-to-workspace 1;
-          "Mod+Shift+2".action = move-column-to-workspace 2;
-          "Mod+Shift+3".action = move-column-to-workspace 3;
-          "Mod+Shift+4".action = move-column-to-workspace 4;
-          "Mod+Shift+5".action = move-column-to-workspace 5;
-          "Mod+Shift+6".action = move-column-to-workspace 6;
-          "Mod+Shift+7".action = move-column-to-workspace 7;
-          "Mod+Shift+8".action = move-column-to-workspace 8;
-          "Mod+Shift+9".action = move-column-to-workspace 9;
-
-          "Mod+Comma".action  = consume-window-into-column;
-          "Mod+Period".action = expel-window-from-column;
-
-          "Mod+BracketLeft".action  = consume-or-expel-window-left;
-          "Mod+BracketRight".action = consume-or-expel-window-right;
-
-          "Mod+R".action = switch-preset-column-width;
-          "Mod+Shift+R".action = switch-preset-window-height;
-          "Mod+Ctrl+R".action = reset-window-height;
           "Mod+F".action = maximize-column;
-          "Mod+Shift+F".action = fullscreen-window;
-          "Mod+C".action = center-column;
-
-          "Mod+Minus".action = set-column-width "-10%";
-          "Mod+Equal".action = set-column-width "+10%";
-
-          "Mod+Shift+Minus".action = set-window-height "-10%";
-          "Mod+Shift+Equal".action = set-window-height "+10%";
-
-          "Print".action = screenshot;
-          "Ctrl+Print".action = screenshot-screen;
-          "Alt+Print".action = screenshot-window;
-
-          "Mod+Shift+E".action = quit;
-
-          "XF86AudioMicMute".action     = spawn "wpctl" "set-mute" "@DEFAULT_AUDIO_SOURCE@" "toggle";
-          "XF86AudioMicMute".allow-when-locked = true;
-
-          "XF86Launch1".action = sh "${lib.getExe pkgs.rofi-rbw-wayland} -a copy -t password --clear-after 20";
-
-          "XF86AudioPrev".action = sh "${lib.getExe pkgs.playerctl} previous";
-          "XF86AudioPlay".action = sh "${lib.getExe pkgs.playerctl} play-pause";
-          "XF86AudioNext".action = sh "${lib.getExe pkgs.playerctl} next";
-
-          # "Mod+V".action = sh "${lib.getExe pkgs.wezterm} start --class 'clipse' -e '${lib.getExe config.modules.desktop.clipse.package}'";
         };
       };
     };
