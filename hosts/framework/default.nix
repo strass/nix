553e524e85fe7ebd113c48dc16f16ebe01cf930b
Zak Strassberg <zakstrassberg@gmail.com>
2025-01-09 23:41:08 -0800

    start framework config

--- hosts/framework/default.nix
+++ hosts/framework/default.nix
@@ -1,6 +1,46 @@
 {
   description = "Config for framework";
 
+imports = [
+    ./hardware.nix
+    ./framework.nix
+    (fetchTarball "https://github.com/nix-community/nixos-vscode-server/tarball/master")
+  ];
+
+  services.vscode-server.enable = true;
+
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/nvme0n1";
+  boot.loader.grub.useOSProber = true;
+
+  networking.hostName = "framework"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  # Enable automatic login for the user.
+  services.getty.autologinUser = "strass";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  modules = {
+    ssh.enable = true;
+
+    # desktop = {
+    #   niri.enable = true;
+    #   waybar.enable = true;
+    #   execOnStart = ["cog http://zaks.pw:2021"];
+    # };
+  };
+
   #  fileSystems."/home/strass/downloads" = {
   #   device = "none";
   #   fsType = "tmpfs";
---
+++ hosts/framework/framework.nix
@@ -0,0 +1,13 @@
+{
+
+  
+}{
+  services.fwupd.enable = true;
+  services.fwupd.extraRemotes = [ "lvfs-testing" ];
+  # Might be necessary once to make the update succeed
+  services.fwupd.uefiCapsuleSettings.DisableCapsuleUpdateOnDisk = true;
+
+  imports = [
+    hardware.framework-11-gen-intel
+  ];
+}
