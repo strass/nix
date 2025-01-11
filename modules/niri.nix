bc1c3359ed31aea4364101cfebcc1b4009c15f85
Zak Strassberg <zakstrassberg@gmail.com>
2025-01-10 16:48:31 -0800

    wip

--- modules/niri.nix
+++ modules/niri.nix
@@ -4,7 +4,6 @@
   pkgs,
   inputs,
   system,
-  stylix,
   ...
 }:
 with lib; let
@@ -22,14 +21,6 @@ in {
   imports = [
     inputs.niri.nixosModules.niri
   ];
-
-  stylix.enable = true;
-  stylix.image = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/ng-hai/hyper-rose-pine-next/refs/heads/main/wallpaper/dark/wallpaper-block-wave/themer-wallpaper-block-wave-dark-5120x2880.png";
-    sha256 = "sha256-Q5ZtrIDtPZKOYohNt9NjPF6suV3rcw1HK8mx7+Ll4Ts=";
-  };
-  stylix.polarity = "dark";
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
 
   config = mkIf cfg.enable {
     hm.home.packages = [
