19ef891d50fe5f5ab26ba07afae309908400b1aa
Zak Strassberg <zakstrassberg@gmail.com>
2025-05-17 01:44:12 -0700

    openbao config

--- services/secrets.openbao.nix
+++ services/secrets.openbao.nix
@@ -1,5 +1,48 @@
-{pkgs, ...}: {
-  environment.systemPackages = [
-    pkgs.openbao
-  ];
+{
+  inputs,
+  pkgs,
+  ...
+}: let
+  domain = "framework.local";
+  name = "openbao";
+  port = 6200;
+  port2 = 6201;
+in {
+  services.openbao = {
+    package = inputs.nixpkgs-unstable.legacyPackages."${pkgs.system}".openbao;
+    enable = true;
+
+    settings = {
+      ui = true;
+      cluster_addr = "https://127.0.0.1:${toString port2}";
+      api_addr = "https://127.0.0.1:${toString port}";
+
+      listener.default = {
+        type = "tcp";
+      };
+
+      storage.raft.path = "/var/lib/openbao";
+    };
+  };
+
+  networking.firewall = {
+    allowedTCPPorts = [
+      port
+      port2
+    ];
+  };
+
+  services.traefik.dynamicConfigOptions = {
+    http.routers."${name}" = {
+      rule = "Host(`${name}.${domain}`)";
+      service = name;
+    };
+    http.services."${name}" = {
+      loadBalancer.servers = [
+        {
+          url = "http://localhost:${toString port}";
+        }
+      ];
+    };
+  };
 }
