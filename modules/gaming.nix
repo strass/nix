75cd6ea9c1e6c5ce68c70f318074b5e2f2c03601
Zak Strassberg <zakstrassberg@gmail.com>
2025-05-17 19:13:12 -0700

    nvidia optimization

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -10,8 +10,46 @@
     extraPackages = with pkgs; [
       vpl-gpu-rt
     ];
-    # graphics.extraPackages = with pkgs; [vaapiIntel intel-media-driver];
+    graphics.enable = true;
+    graphics.extraPackages = with pkgs; [vaapiIntel intel-media-driver];
+
+    nvidia = {
+      # Modesetting is required.
+      modesetting.enable = true;
+
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      powerManagement.enable = false;
+      # Fine-grained power management. Turns off GPU when not in use.
+      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+      powerManagement.finegrained = false;
+
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of
+      # supported GPUs is at:
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+      # Only available from driver 515.43.04+
+      # Currently alpha-quality/buggy, so false is currently the recommended setting.
+      # NOTE: I have a 1080 in here so this is set to false.
+      open = false;
+
+      # Enable the Nvidia settings menu,
+      # accessible via `nvidia-settings`.
+      nvidiaSettings = true;
+
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+      # prime = {
+      #   # Make sure to use the correct Bus ID values for your system!
+      #   intelBusId = "PCI:0:2:0";
+      #   nvidiaBusId = "PCI:14:0:0";
+      #   # amdgpuBusId = "PCI:54:0:0"; For AMD GPU
+      # };
+    };
   };
+
+  services.xserver.videoDrivers = ["nvidia"];
 
   programs = {
     appimage = {
