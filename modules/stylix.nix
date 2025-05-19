4148638f78f487379631e441e563b75eb683b9f2
Zak Strassberg <zakstrassberg@gmail.com>
2025-05-18 21:49:21 -0700

    test

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -3,11 +3,13 @@
   lib,
   ...
 }: {
-  stylix.enable = true;
-  stylix.image = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/ng-hai/hyper-rose-pine-next/refs/heads/main/wallpaper/dark/wallpaper-block-wave/themer-wallpaper-block-wave-dark-5120x2880.png";
-    sha256 = "sha256-Q5ZtrIDtPZKOYohNt9NjPF6suV3rcw1HK8mx7+Ll4Ts=";
+  stylix = {
+    enable = true;
+    image = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/ng-hai/hyper-rose-pine-next/refs/heads/main/wallpaper/dark/wallpaper-block-wave/themer-wallpaper-block-wave-dark-5120x2880.png";
+      sha256 = "sha256-Q5ZtrIDtPZKOYohNt9NjPF6suV3rcw1HK8mx7+Ll4Ts=";
+    };
+    polarity = lib.mkDefault "dark";
+    base16Scheme = lib.mkDefault "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
   };
-  stylix.polarity = lib.mkDefault "dark";
-  stylix.base16Scheme = lib.mkDefault "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
 }
