caba5736e70ff66df54ae9448ae0d0e0f48d433c
Zak Strassberg <zakstrassberg@gmail.com>
2025-05-23 12:54:00 -0700

    config for router

---
+++ hosts/router/default.nix
@@ -0,0 +1,76 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+{
+  config,
+  pkgs,
+  pkgs-unstable,
+  inputs,
+  ...
+}: {
+  imports = [
+    ./hardware.nix
+    inputs.nixos-facter-modules.nixosModules.facter
+
+    inputs.disko.nixosModules.disko
+    ./disk-config.nix
+    ../../modules/gnome.nix
+    ../../modules/stylix.nix
+
+    ../../services/avahi.nix
+  ];
+
+  facter.reportPath = ./facter.json;
+  networking.useNetworkd = false; # using facter without this fails
+
+  # Bootloader.
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    kernelPackages = pkgs-unstable.linuxPackages_latest;
+    kernelParams = ["pcie_aspm.policy=performance"];
+  };
+
+  networking.hostName = "router"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  programs.dconf.enable = true;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    pkgs-unstable.ghostty
+    gnome-tweaks
+  ];
+
+  modules = {
+    ssh.enable = true;
+  };
+
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-estuary.yaml";
+}
