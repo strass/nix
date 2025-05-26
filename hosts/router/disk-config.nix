0e6a53f403fdd89b824f9f2d46275ec1ce8c96ca
Zak Strassberg <zakstrassberg@gmail.com>
2025-05-25 23:02:24 -0700

    router disk config

--- hosts/router/disk-config.nix
+++ hosts/router/disk-config.nix
@@ -1,51 +1,68 @@
-# Example to create a bios compatible gpt partition
-{lib, ...}: {
+{
   disko.devices = {
-    disk.disk1 = {
-      device = lib.mkDefault "/dev/nvme0n1";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          boot = {
-            name = "boot";
-            size = "1M";
-            type = "EF02";
-          };
-          esp = {
-            name = "ESP";
-            size = "500M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              start = "1MiB";
+              end = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
             };
-          };
-          root = {
-            name = "root";
-            size = "100%";
-            content = {
-              type = "lvm_pv";
-              vg = "pool";
+            swap = {
+              start = "1G";
+              end = "9G";
+              content = {
+                type = "swap";
+                randomEncryption = true;
+              };
             };
-          };
-        };
-      };
-    };
-    lvm_vg = {
-      pool = {
-        type = "lvm_vg";
-        lvs = {
-          root = {
-            size = "100%FREE";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/";
-              mountOptions = [
-                "defaults"
-              ];
+            root = {
+              start = "9G";
+              end = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = ["-f"];
+                subvolumes = {
+                  "@" = {};
+                  "@/root" = {
+                    mountpoint = "/";
+                    mountOptions = ["compress=zstd" "noatime"];
+                  };
+                  "@/home" = {
+                    mountpoint = "/home";
+                    mountOptions = ["compress=zstd"];
+                  };
+                  "@/nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = ["compress=zstd" "noatime"];
+                  };
+                  "@/persist" = {
+                    mountpoint = "/persist";
+                    mountOptions = ["compress=zstd" "noatime"];
+                  };
+                  "@/var-lib" = {
+                    mountpoint = "/var/lib";
+                    mountOptions = ["compress=zstd" "noatime"];
+                  };
+                  "@/var-log" = {
+                    mountpoint = "/var/log";
+                    mountOptions = ["compress=zstd" "noatime"];
+                  };
+                  "@/var-tmp" = {
+                    mountpoint = "/var/tmp";
+                    mountOptions = ["compress=zstd" "noatime"];
+                  };
+                };
+              };
             };
           };
         };
