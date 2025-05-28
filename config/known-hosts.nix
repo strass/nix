dfc448129ed0e1540d2002bbbb38be42805dec89
Zak Strassberg <zakstrassberg@gmail.com>
2025-05-28 14:07:01 -0700

    update known hosts

--- config/known-hosts.nix
+++ config/known-hosts.nix
@@ -15,12 +15,12 @@
       ip = "192.168.1.10";
     };
     framework = {
-      publicKeys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDyrhHvlLOPJ1UF7r5QgytPa9GHwObXXkQdH/VAJXB4+ zakstrassberg@gmail.com"];
+      publicKeys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDyrhHvlLOPJ1UF7r5QgytPa9GHwObXXkQdH/VAJXB4+ zakstrassberg@gmail.com" "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCjzA46DOhnvOtdOK/KdVPeQvbD7UyLyzMNAktGjDE3deX3thO/Q/9tgT89mK4SH52lHm/bcS6fg/RzvC0d7kbuqjw/l/LYCV/ZK0uEhXqk1vrZzCTEhL5o7kv7ZQAkoCdwVdZdTgAffdvhMDXKBtUNtLp/GnTCrF7ck0T/wn7kYJLoOn1pGQJ7Y2acwOGJAv60aI8b14LRO3nvrzrVFqPoZcosDT9rknBnxlLIIGLz/UmhzOQeSi7Bp2RhLzoXPiLcMS3oNxLeGvks+1KuAXmsMlLLLXCGu0Pkf5uaEWwIyk9B9fv19YEEsV5h3LcdM3j7P9VJ0SlxX5odOBZrREydyR8/I6oKauYs8E/Qc7IWqRXGb5xQAZkseZOl5z1JgzATuwgFqDIfBxg13gPpMry8r6N/U+zIhK1O7OA4iCmpgaqmkRtmgQmRU0NZ2/Ad3KFKTSHHpxgNRy8q7d9bL3EJUBsF6PbPvr50dfdH7go6xur2DwVzQFa3osOEeAC29ucEfpsYQ2Bf5Recvo5pj9pPST3nfH/gYYwlgBDfva+acTioee8dyAMo8ptoTyxAN2Xf6zgVY9GNF/E7c6QfzkuW5/32amWCjlsmjZuDvT47NEa20m1qk5+QTSfYrQNGQ1UDpwF+vBOlATRBFt+WgGZynNZsVW8Z9RfeNfsRNOdudw=="];
     };
     gamer = {
       hostName = "gamer";
       userName = "strass";
-      publicKeys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILG/pdJKfHUsHVD04Fx87BwCyHeBg5Z57AjLUxT4rTh2 strass@gamer"];
+      publicKeys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHM5BreWIM2ffeI9+evDQECrEOPPwZ5qKwTq+WwGb2mJ" "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC8OyPDvEgUYHbouf1pxiz1T5U9y/AE/G5pJQKz9RAcagjLa7zrHFTGHbm6B2LEmCIoS4PWUdhO03q8boTGqqQp4DGA/3Da8hKQJmkZf0tGYmte+8E3gphwHGYOm8kSI201jfX8M7fy+XXpc+Ib3OTfsFx37iwLaZ0pn83MAnBr3MpuIWnHZ6FYJzIUsbE8a8+dZtTFfk6EaebVbnoP9lwKgyRWOz3s323A6239WZ6GO85meDLkJqD/CZMFPgbQhgmL3FoyNy2sOJL7305dqWRGlvPd+ojea5cirJgQ/Yu8Qz39bj+fnnpLjES5+0kTJl5quModPHVa/nafi2yHmH60urpbVwCX4RRBphGXrASAMfNirZlCdMCjfl/H+Dh83PEXl6SZg95fsF2P4po1Fkr9Gd4r6UVXtRqK0QZdymcjDI84AdGnb/PYZP8HW1JACXnhPREOOW2W/t41GAytH6qDR/yKMMaqUGx1lEZ62aSAMhEsoQUoJfx4gri1TNwuAfugLkkp9m/l+j7YIBaBNtl3SnvpbbWbToXzAfTRErRMTwgegk4fjpOQ2xeBUR3/i5AjolxzsoSihYIC3RuTgRrD20DpMlWJknQI+FTSBQatC/vMHj6mQEY1/0VfD7iDVcRaWV8wSVJcFVvC+hOXUBli1edaPr0M5htuBMpuFPuYvQ=="];
     };
   };
 
