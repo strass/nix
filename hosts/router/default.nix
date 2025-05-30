7c2e540afe10e41035b6e6d426e595828fcaa033
Zak Strassberg <zakstrassberg@gmail.com>
2025-05-30 16:22:54 -0700

    modularize

--- hosts/router/default.nix
+++ hosts/router/default.nix
@@ -13,6 +13,8 @@
     ../../modules/de.gnome.nix
     ../../modules/stylix.nix
     ../../modules/podman.nix
+    ../../modules/fs.snapshots.nix
+    ../../modules/fs.impermanence.nix
 
     ../../services/traefik.nix
     ../../services/nix.cache.nix
@@ -36,73 +38,6 @@
 
   modules = {
     ssh.enable = true;
-  };
-
-  # A snapshot is not a backup: snapshots work by use of BTRFS’ copy-on-write behaviour.
-  # A snapshot and the original it was taken from initially share all of the same data blocks.
-  # If that data is damaged in some way (cosmic rays, bad disk sector, accident with dd to the disk),
-  # then the snapshot and the original will both be damaged.
-  #
-  # Snapshots are useful to have local online “copies” of the filesystem that can be referred back to,
-  # or to implement a form of deduplication, or to fix the state of a filesystem for making a full backup
-  # without anything changing underneath it. They do not in themselves make your data any safer.
-  services.snapper = {
-    snapshotRootOnBoot = true;
-    configs = {
-      home = {
-        SUBVOLUME = "/home";
-        ALLOW_USERS = ["strass"];
-        TIMELINE_CREATE = true;
-        TIMELINE_CLEANUP = true;
-      };
-    };
-  };
-  fileSystems."/persist" = {
-    device = "/dev/disk/by-partlabel/disk-nvme0n1-root";
-    neededForBoot = true;
-    fsType = "btrfs";
-    options = ["subvol=persist" "compress=zstd" "noatime"];
-  };
-  environment.persistence."/persist" = {
-    hideMounts = true;
-    directories = [
-      # "/var/log"
-      # "/var/lib/bluetooth"
-      # "/var/lib/nixos"
-      # "/var/lib/systemd/coredump"
-      "/etc/NetworkManager/system-connections"
-      # {
-      #   directory = "/var/lib/colord";
-      #   user = "colord";
-      #   group = "colord";
-      #   mode = "u=rwx,g=rx,o=";
-      # }
-    ];
-    files = [
-      "/etc/machine-id"
-    ];
-    users.strass = {
-      directories = [
-        "Downloads"
-        "Documents"
-        {
-          directory = ".gnupg";
-          mode = "0700";
-        }
-        {
-          directory = ".ssh";
-          mode = "0700";
-        }
-        {
-          directory = ".local/share/keyrings";
-          mode = "0700";
-        }
-        ".local/share/direnv"
-      ];
-      files = [
-        ".screenrc"
-      ];
-    };
   };
 
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-estuary.yaml";
--- modules/fs.impermanence.nix
+++ modules/fs.impermanence.nix
@@ -1,12 +1,59 @@
+{}: {
+  fileSystems."/persist" = {
+    device = "/dev/disk/by-partlabel/disk-nvme0n1-root";
+    neededForBoot = true;
+    fsType = "btrfs";
+    options = ["subvol=persist" "compress=zstd" "noatime"];
+  };
+  environment.persistence."/persist" = {
+    hideMounts = true;
+    directories = [
+      # "/var/log"
+      # "/var/lib/bluetooth"
+      # "/var/lib/nixos"
+      # "/var/lib/systemd/coredump"
+      "/etc/NetworkManager/system-connections"
+      # {
+      #   directory = "/var/lib/colord";
+      #   user = "colord";
+      #   group = "colord";
+      #   mode = "u=rwx,g=rx,o=";
+      # }
+    ];
+    files = [
+      "/etc/machine-id"
+    ];
+    users.strass = {
+      directories = [
+        "Downloads"
+        "Documents"
+        {
+          directory = ".gnupg";
+          mode = "0700";
+        }
+        {
+          directory = ".ssh";
+          mode = "0700";
+        }
+        {
+          directory = ".local/share/keyrings";
+          mode = "0700";
+        }
+        ".local/share/direnv"
+      ];
+      files = [
+        ".screenrc"
+      ];
+    };
+  };
+}
 # #!/usr/bin/env bash
 # # fs-diff.sh
 # # https://serverfault.com/questions/399894/does-btrfs-have-an-efficient-way-to-compare-snapshots
 # # sudo mkdir /mnt ; sudo mount -o subvol=/ /dev/sda3 /mnt ; ./fs-diff.sh
 # set -euo pipefail
-
 # OLD_TRANSID=$(sudo btrfs subvolume find-new /mnt/root-blank 9999999)
 # OLD_TRANSID=${OLD_TRANSID#transid marker was }
-
 # sudo btrfs subvolume find-new "/mnt/root" "$OLD_TRANSID" |
 # sed '$d' |
 # cut -f17- -d' ' |
@@ -22,3 +69,4 @@
 #     echo "$path"
 #   fi
 # done
+
