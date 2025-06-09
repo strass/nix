48eac4f95fddcc44545b5e8c8c094fca9cc68df2
Zak Strassberg <zakstrassberg@gmail.com>
2025-06-09 14:25:50 -0700

    add disk-config

---
+++ hosts/beelink/disk-config.nix
@@ -0,0 +1,73 @@
+{
+  disko.devices = {
+    disk = {
+      sda = {
+        type = "disk";
+        device = "/dev/sda";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              priority = 1;
+              name = "ESP";
+              start = "1MiB";
+              end = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = ["umask=0077"];
+              };
+            };
+            swap = {
+              size = "8G";
+              content = {
+                type = "swap";
+                randomEncryption = true;
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = ["-f"];
+                subvolumes = {
+                  "@" = {};
+                  "@/root" = {
+                    mountpoint = "/";
+                    mountOptions = ["compress=zstd" "noatime"];
+                  };
+                  "@/home" = {
+                    mountpoint = "/home";
+                    mountOptions = ["compress=zstd"];
+                  };
+                  "@/nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = ["compress=zstd" "noatime"];
+                  };
+                  "@/persist" = {
+                    mountpoint = "/persist";
+                    mountOptions = ["compress=zstd" "noatime"];
+                  };
+                  "@/var-lib" = {
+                    mountpoint = "/var/lib";
+                    mountOptions = ["compress=zstd" "noatime"];
+                  };
+                  "@/var-log" = {
+                    mountpoint = "/var/log";
+                    mountOptions = ["compress=zstd" "noatime"];
+                  };
+                  "@/var-tmp" = {
+                    mountpoint = "/var/tmp";
+                    mountOptions = ["compress=zstd" "noatime"];
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
