14a64d4d428a0beb8d38bb4a4021836480077eba
Zak Strassberg <zakstrassberg@gmail.com>
2025-06-12 23:54:47 -0700

    add self-hosted substituter

--- flake.nix
+++ flake.nix
@@ -72,6 +72,8 @@
         "https://hyprland.cachix.org"
         "https://nix-gaming.cachix.org"
         "https://nix-citizen.cachix.org"
+
+        "http://nix-cache.router.local"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
@@ -80,6 +82,8 @@
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
         "nix-citizen.cachix.org-1:lPMkWc2X8XD4/7YPEEwXKKBg+SVbYTVrAaLA2wQTKCo="
+
+        "nix-cache.router.local:FdMJI11DQtnf3jk18u/jDj0kOySK1CbjKACQlt3gtZk="
       ];
     };
 
--- services/nix.cache.nix
+++ services/nix.cache.nix
@@ -11,7 +11,7 @@
 in {
   services.nix-serve = {
     enable = true;
-    secretKeyFile = "/home/strass/.ssh/nix-cache-key";
+    secretKeyFile = "/home/strass/.ssh/nix-store.secret"; # public: nix-cache.router.local:FdMJI11DQtnf3jk18u/jDj0kOySK1CbjKACQlt3gtZk=
     port = port;
     openFirewall = true;
   };
